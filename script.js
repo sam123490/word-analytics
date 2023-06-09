604627449a874e4de9c97f533a554d4bf71234ea
Samuel Rodriguez <sam.rod.719@gmail.com>
2023-06-09 14:15:52 -0700

    cleaner code and validation

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ const twitterNumberEl = document.querySelector('.stat__number--twitter');
 const facebookNumberEl = document.querySelector('.stat__number--facebook');
 const wordsNumberEl = document.querySelector('.stat__number--words');
 
-textareaEl.addEventListener('input', () => {
+const inputHandler = () => {
+    //input validation
+    if (textareaEl.value.includes('<script>')) {
+        alert('you can\'t use <script> ...');
+        textareaEl.value = textareaEl.value.replace('<script>', '=)');
+    }
+
     // determine new numbers
     let numberOfWords = textareaEl.value.split(' ').length;
     if (textareaEl.value.length === 0) {
@@ -27,10 +33,11 @@ textareaEl.addEventListener('input', () => {
         facebookNumberEl.classList.remove('stat__number--limit');
     }
 
-    
     // set new numbers
     wordsNumberEl.textContent = numberOfWords;
     charactersNumberEl.textContent = numberOfCharacters;
     twitterNumberEl.textContent = twitterCharsLeft;
     facebookNumberEl.textContent = facebookCharsLeft; 
-})
+};
+
+textareaEl.addEventListener('input', inputHandler);
